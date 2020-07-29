8b4f455b963a3e0fcf6d269d3b2566d6d3850e11
Sumaiya Chowdhury Muntaha <sumyiachowdhury1010@gmail.com>
2020-07-29 16:24:39 +0600

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-//hw -> leapyear check
+let year= ("1800");
+let check= ("year%4");
+let secondcheck=("year%100");
+let thirdcheck=("year%400");
+if(check==0)if(thirdcheck==0){
+    console.log(yes);
+}else if(secondcheck==0)
+    console.log(yes);
+}else{
+    console.log(no);
+}
