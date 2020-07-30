d33610f6b43e2879006a8f73c76829af5777c419
Sumaiya Chowdhury Muntaha <sumyiachowdhury1010@gmail.com>
2020-07-30 10:23:16 +0600

    hw complete

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-let year= ("1800");
-let check= ("year%4");
-let secondcheck=("year%100");
-let thirdcheck=("year%400");
-if(check==0)if(thirdcheck==0){
-    console.log(yes);
-}else if(secondcheck==0)
-    console.log(yes);
-}else{
-    console.log(no);
-}
+let year = 2000;
+let firstCheck = year % 4
+let secondCheck = year % 100
+let thirdcheck = year % 400
+if(secondCheck == 0 ){
+    if(firstCheck == 0){
+       console.log("yes");}
+    }else if(thridcheck == 0){
+        console.log("yes");
+    }else{
+        console.log("no");
+    }
